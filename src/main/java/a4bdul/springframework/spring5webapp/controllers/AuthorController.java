4abfa4248c6646efd46ccf463ef4feb4111f4a5c
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2018-06-09 12:56:38 +0100

    add controller and thymeleaf to display author list and book list

---
+++ src/main/java/a4bdul/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,26 @@
+package a4bdul.springframework.spring5webapp.controllers;
+
+import a4bdul.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by 4bdul on 09/06/2018 at 12:49 PM.
+ */
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors",authorRepository.findAll());
+
+        return "authors";
+    }
+}
